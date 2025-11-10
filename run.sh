f2a8500095658f15625274d6ca02f74df42fb1d2
simonguillo <simonguillo@gmail.com>
2025-11-10 12:26:35 +0100

    added temporary directory creation

--- run.sh
+++ run.sh
@@ -2,3 +2,7 @@ now=$(date +"%Y-%m-%dT%H:%M:%S.%3N%z")
 echo "Bash script starting at: $now" 
 
 echo "Script full path: '$(realpath run.sh)'"
+
+temp_dir="temp_dir.temp"
+[ -d "$temp_dir" ] && rm -rf "$temp_dir"
+mkdir -p "$temp_dir"
